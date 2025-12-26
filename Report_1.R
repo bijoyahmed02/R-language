bf29455725d5a1987359860b37819ecf4fa5038f
Bijoy Ahamed <bijoyahmed818@gmail.com>
2025-12-26 22:22:53 +0600

    Basic syntax, R Data Structures

---
+++ Report_1.R
@@ -0,0 +1,309 @@
+#vectors...........
+
+num_vec <- c(1, 2, 3, 4, 5)
+print(num_vec)
+
+char_vec <- c("hello world")
+print(char_vec)
+
+log_vec <- c(TRUE, FALSE)
+print(log_vec)
+
+#vector operation
+
+vec1 <- c(2,4,6)
+vec2 <- c(1,3,5)
+
+sum_vec <- vec1 + vec2
+prod_vec <- vec1 * vec2
+print(sum_vec)
+print(prod_vec)
+
+#Accessing elements in a vector
+
+num_vec <- c(10,20,30,40,50)
+print(num_vec[2])
+print(num_vec[c(1,3,5)]) 
+print(num_vec[num_vec > 25])
+
+#modify vector
+num_vec[2] <-100
+print(num_vec)
+
+num_vec <- c(num_vec,60,70)
+print(num_vec)
+
+#vector Funcions
+vec <- c(5,10,15,20,25)
+print(length(vec))
+print(sum(vec))
+print(mean(vec))
+sorted_vec <- sort(vec,decreasing = TRUE)
+print(sorted_vec)
+
+#sequence and repetition
+seq_vec <- seq(1,10, by=2)
+print(seq_vec)
+
+#repeat elements
+rep_vec <- rep(c(1,2,3), times=3)
+print(rep_vec)
+
+#metrics
+mat <- matrix(1:9, nrow =3, ncol =3)
+print(mat)
+mat <- matrix(1:9, nrow =3, byrow =TRUE)
+print(mat)
+
+mat <- matrix(1:9, nrow =3)
+rownames(mat) <- c("Row1","Row2","Row3")
+colnames(mat) <- c("Col1","Col2","Col3")
+print(mat)
+
+mat <- matrix(1:9, nrow =3)
+print(mat[2,3])
+
+print(mat[1, ])
+print(mat[,2])
+
+mat1 <- matrix(1:4, nrow =2)
+mat2 <- matrix(5:8, nrow =2)
+
+sum_mat <- mat1 + mat2
+print(sum_mat)
+
+prod_mat <- mat1 * mat2
+print(prod_mat)
+
+dot_prod_mat <- mat1 %*%mat2
+print(dot_prod_mat)
+
+t_mat <- t(mat)
+print(t_mat)
+
+square_mat <- matrix(c(2,3,1,4),nrow =2)
+inv_mat <-solve(square_mat)
+print(inv_mat)
+
+
+#Arrays
+arr <- array(1:18,dim = c(3,3,2))
+print(arr)
+
+arr <- array(1:18,dim = c(3,3,2))
+print(arr[2,3,1]) 
+
+arr1 <- array(1:18,dim = c(3,3,2))
+arr2 <- array(19:36, dim =c(3,3,2))
+sum_arr <- arr1 + arr2
+print(sum_arr)
+
+arr <- array(1:18, dim = c(3,3,2))
+print(sum(arr))
+
+#Data Frames
+df <- data.frame( 
+ID = c(101,102,103,104), 
+Name = c("Alice","Bob","Charlie","David"), 
+Age = c(23,25,22,24), 
+Score = c(89.5,76.0,91.2,88.8),
+Passed = c(TRUE,FALSE,TRUE,TRUE)
+)
+print(df)
+
+
+print(df$Name)
+
+print(df [2, ])
+
+print (df [3,])
+
+
+print(df[3,"Score"])
+
+
+print(df[4,"Age"])
+
+
+print(df[,c("Name", "Score")])
+
+print (df[,c("Name","Age")])
+
+# Access multiple rows
+print (df[1:2, ]) 
+
+print (df[2:4, ]) 
+
+# Add new column 'Grade'
+df$Grade <- c("A","B","A+","A-")
+print (df)
+
+df$City <-c ("Dhaka","Chittagong","Sylhet","Feni")
+print (df)
+
+# Filter students who passed
+passed_students <- df [df$Passed == TRUE, ]
+print (passed_students)
+
+# Filter students with Score > 85
+high_scorers <- df[df$Score >85, ]
+print(high_scorers)
+
+#Sorting by Age (Ascending)
+df_sorted <- df[order(df$Age),]
+print (df_sorted)
+
+#Sorting by Age (Descending)
+df_sorted_desc <- df[order(-df$Age),]
+print (df_sorted_desc)
+
+df_sorted_desc_Score <- df[order(-df$Score),]
+print (df_sorted_desc_Score)
+
+df$Score[df$Name =="Bob"] <-80
+print(df)
+
+colnames(df) <- c("Student_ID","Student_Name","Student_Age","Exam_Score",
+                  "Passed_Exam","Final_Grade","City Name")
+print(df)
+
+# Remove column
+df$Grade <-NULL
+print(df)
+df <- df[-2, ]
+print(df)
+df <- df[,-"Grade" ]
+print(df)
+summary(df)
+
+# List 
+my_list <- list(
+  Name = "Alice",
+  Age =25,
+  Scores = c(90,85,88),
+  Passed = TRUE
+)
+print (my_list)
+print (my_list[[1]]) 
+
+print (my_list[[3]]) 
+print (my_list$Scores)
+print (my_list$Age)
+
+# Access specific elements within a list item
+print (my_list$Scores[2]) 
+
+# Change an element
+my_list$Age <- 26
+print (my_list$Age) 
+
+# Add a new element
+my_list$Country <- "USA"
+print (my_list)
+
+# Remove an element/full row remove
+my_list$Passed <- NULL
+print (my_list)
+
+my_list$Passed <- TRUE
+print (my_list)
+
+my_list$Scores <- c(30,80,90,98)
+print (my_list)
+
+my_list$Scores <- c(30)
+print (my_list)
+
+#Creating a list with a matrix and a data frame
+my_complex_list <-list(
+  Number = c(1,2,3,4,5),
+  Matrix = matrix(1:9,nrow = 3),
+  DataFrame = data.frame(ID = c(101,102),Name = c("Bob","Charlie"))
+)
+print(my_complex_list)
+print(my_complex_list$Matrix[2,3])
+
+print(my_complex_list$Matrix[2,])
+print(my_complex_list$Matrix[,3])
+# Marge the list
+list1 <- list(A =1 : 5 , B =  "Hello")
+list2 <- list(C = c( TRUE , FALSE),
+ 
+              D = matrix( 1 : 4,nrow = 2))
+# Merge lists
+merged_list <- c(list1, list2)
+print(merged_list)
+
+# Convert list to data frame
+list_to_df <- data.frame( Name = c( "Alice" , "Bob"),
+                          Age = c(25,27),
+                          Score = c(89,92))
+print(list_to_df) 
+
+
+# Exercise 1:
+
+Score_vec <- c(70,75,80,90,87,73,85,89)
+print(Score_vec)
+print (min(Score_vec))
+print(mean(Score_vec))
+print (max(Score_vec))
+print (median(Score_vec))
+cities <- c("Dhaka","Kushtia","Jhenaidah","Khulna","Bagura")
+print (cities)
+
+# Exercise 2:
+student_info <- list (
+  name = "Bijoy",
+  age = 24,
+  grade = c(90,85,89)
+)
+print(student_info)
+print (student_info$age)
+print (student_info$grade[2])
+
+# Exercise 3:
+mat <- matrix(1:16, nrow =4, ncol =4)
+print(mat)
+print(mat[3,2])
+
+#Exercise 4:
+students <- data.frame(
+  Name    = c("Alice", "Bob", "Charlie", "David"),
+  Math    = c(85, 78, 92, 88),
+  English = c(90, 82, 95, 87)
+)
+str(students)
+print(students$English)
+students$Average <- rowMeans(students[, c("Math", "English")])
+print(students)
+
+# Exercise 5:
+blood_group <- factor(c("A", "B", "O", "AB", "A", "O"))
+print(levels(blood_group))
+print(table(blood_group))
+
+# Exercise 6:
+class <- data.frame(
+  Name    = c("Ava", "Ben", "Chloe", "Dylan", "Ethan"),
+  Age     = c(20, 21, 22, 20, 23),
+  Gender  = factor(c("Female", "Male", "Female", "Male", "Male")),
+  Score1  = c(82, 75, 90, 78, 85),
+  Score2  = c(88, 80, 92, 81, 79),
+  Score3  = c(84, 77, 89, 83, 88)
+  
+)
+class$Average <- rowMeans(class[, c("Score1", "Score2", "Score3")])
+print(class)
+high_achievers <- class$Name[class$Average > 80]
+print(high_achievers)
+print(table(class$Gender))                          
+              
+
+
+
+
+
+
+
+
